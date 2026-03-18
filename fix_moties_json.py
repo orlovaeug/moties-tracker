583e96eac42eac961caeadebc4951b5ba66d4471
orlovaeug <orlova.eug@gmail.com>
2026-03-18 23:29:08 +0100

    Update fix_moties_json.py

--- fix_moties_json.py
+++ fix_moties_json.py
@@ -85,7 +85,7 @@ LEDEN_PARTIJ = {
     "Keijzer":"Groep-Keijzer",
     "Baay-Timmerman":"50PLUS",
     # Extra members seen in recent moties
-    "Lahlah":"GL-PvdA", "Clemminck":"JA21", "de Hoop":"GL-PvdA","Nobel":"VVD","Mooiman":"PVV", "Kostic":"PvdD", "Jansen":"PVV", "Koorevaar":"CDA", "Goudzwaard":"JA21",
+    "Lahlah":"GL-PvdA","Stoteler":"PVV", "Van der Lee":"GL-PvdA","Van Brenk":"50PLUS","Hoogeven":"JA21","Neijenhuis":"D66","Clemminck":"JA21", "Boomsma":"JA21","de Hoop":"GL-PvdA","Nobel":"VVD","Mooiman":"PVV", "Kostic":"PvdD", "Jansen":"PVV", "Koorevaar":"CDA", "Goudzwaard":"JA21",
     "Tijs van den Brink":"CDA","Van den Brink":"CDA",
 }
 
